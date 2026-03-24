0f73889376ae05ecfe6886d73283c97bf08d1b09
7AdnVr <abokase4706@gmail.com>
2026-03-24 16:28:53 -0400

    v3

--- 7. Ejercicio 7.py
+++ 7. Ejercicio 7.py
@@ -2,5 +2,5 @@ edad = 19
 edad_10 = edad + 10
 edad_result = edad + (2050 - 2026)
 
-print(f"Si tienes {edad} años en el 2026, tendras en el año 2050: {edad_result}")
-print(f"En 2050 tendre: {edad_result} años")
+print(f"Tengo {edad} años en el 2026, tendre en el año 2050: {edad_result}")
+print(f"En 2050 tengo: {edad_result} años")
---
+++ prueba.py
@@ -0,0 +1,5 @@
+edad = 20
+cal = edad + 10
+futuro = edad + (2050 - 2026)
+print(f"Mi edad es: {edad} y en 10 años tendre: {cal} años")
+print(f"Mi edad en 2050 será: {futuro} años")
