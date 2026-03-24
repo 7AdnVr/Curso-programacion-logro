15f0541721764f4407c61b459713d656d434a190
7AdnVr <abokase4706@gmail.com>
2026-03-24 14:26:50 -0400

    v2

--- 1. Ejercicio 1.py
+++ 1. Ejercicio 1.py
@@ -1,4 +1,6 @@
 nombre_usuario = "Adrian"
 edad = 19
 hobby = "jugar counter strike 2"
-print(f"El nombre del usuario es {nombre_usuario}, tengo {edad} años y me gusta {hobby}")
+color_favorito = "azul"
+
+print(f"El nombre del usuario es {nombre_usuario}, tengo {edad} años y me gusta {hobby}. Mi color favorito es {color_favorito}.")
