33db4fdda7ac1adfa2deb601e5b98d5c4c887845
7AdnVr <abokase4706@gmail.com>
2026-03-24 16:06:16 -0400

    v3

--- 3. Ejercicio 3.py
+++ 3. Ejercicio 3.py
@@ -4,7 +4,10 @@ suma = num1 + num2
 resta =num1 - num2
 multiplicacion = num1 * num2
 division = num1 / num2 
+division_entera = num1 // num2
+modulo = num1 % num2
 print(f"la resta de {num1} y {num2} es: {resta}")
 print(f"la multiplicacion de {num1} y {num2} es: {multiplicacion}")
 print(f"la division de {num1} y {num2} es: {division}")
-print(f"la suma de {num1} y {num2} es: {suma}")
+print(f"la division entera de {num1} y {num2} es: {division_entera}")
+print(f"el modulo de {num1} y {num2} es: {modulo}")
--- 4. Ejercicio 4.py
+++ 4. Ejercicio 4.py
@@ -1,4 +1,4 @@
-tasa_usd = "460"
+tasa_bs = "460"
 cantidad_usd = int(input("Ingrese la cantidad de USD a convertir:"))
-cantidad = cantidad_usd * int(tasa_usd)
-print(f"{cantidad_usd} USD equivalen a {cantidad} Bs")
+cantidad = cantidad_usd * int(tasa_bs)
+print(f"{cantidad_usd} USD a {cantidad} Bs")
