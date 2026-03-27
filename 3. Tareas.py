dc657ca426a2984a87a093a3534e3292dcda0d83
7AdnVr <abokase4706@gmail.com>
2026-03-26 22:32:43 -0400

    v2

--- 3. Tareas.py
+++ 3. Tareas.py
@@ -23,14 +23,54 @@ else:
         print("Error, ingrese un numero valido")  
 
 # Ejercicio 3
+
 edad1 = int(input("Ingrese la edad de la primera persona: "))
 mayor_edad = 18 >=         
+
+
 """
+# Ejercicio 2
+'''
+num1 = int(input(f'Cual es su primer numero: '))
+num2 = int(input(f'Cual es su segundo numero: '))
+
+if num1 > num2:
+   print(f'{num1} Es mayor')
+
+elif num2 > num1:
+   print(f'{num2} Es mayor')
+
+else:
+    print(f'Los dos son iguales')
+
+# Ejercicio 3 Clasificación de Edad
+edad1 = int(input(f'Ingrese su edad > '))
+
+if num1 >= 18:
+    print(f'Tienes {edad1}, eres mayor de edad')
+else:
+ print(f'Tienes {edad1}, eres menor de edad')
+'''
+
+'''
+# Ejercicio 4
+compra = float(input(f'Ingrese el monto final de su compra: '))
+descuento = compra * 0.15
+precio_final = compra - descuento
+
+if compra > 1000:
+    print(f'Ha recibido un descuento de 15%, paga en total: {precio_final}')
+else:
+    print(f'Usted ha pagado el precio regular: {compra}')
 
 # Ejercicio 5
 
+year = int(input(f'Ingrese el año: '))
 
-
+ (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0)
+'''
+# Ejercicio 8
+'''
 #Ejercicio 9
 print("\nBienvenido al programa de becas universitarias.")
 
@@ -43,3 +83,4 @@ if (promedio >= 8.5 and ingresos < 2000) and not reportes_disciplinarios:
 
 else:
     print("\nEl estudiante no es elegible para la beca.")
+'''
