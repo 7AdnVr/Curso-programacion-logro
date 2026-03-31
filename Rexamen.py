ed20e0e2c2cd46b98f295aca713b0cf2f22c4bf6
7AdnVr <abokase4706@gmail.com>
2026-03-30 23:07:09 -0400

    v2

---
+++ Rexamen.py
@@ -0,0 +1,185 @@
+
+import random
+
+
+frase = input("Ingrese una frase -> ")
+
+h1 = ("a", "e", "i", "o", "u")
+
+vocal = 0
+conson = 0
+
+for letra in frase:
+
+    if letra.lower() in h1:
+        vocal += 1
+
+    elif letra.lower():
+        conson += 1
+
+print(f"tiene {vocal} vocales y {conson} consonantes")
+
+# Ejercicio 2
+
+numero = random.randint(1, 100)
+
+usuario = int(input("Intenta adivinar el numero -> "))
+
+while usuario != numero:
+
+    if usuario > numero:
+        print("El numero es muy alto, baja un poco")
+
+    elif usuario < numero:
+        print("El numero es muy bajo, sube un poco")
+     
+
+    usuario = int(input("Vuelva elegir el numero -> "))
+
+
+print("Ha adinivado el numero")
+
+
+# Ejercicio 3
+
+
+lista = [] 
+
+suma = 0
+
+usuario5 = input('Ingrese un numero o ""fin"" si quiere terminar -> ')
+
+while usuario5 != "fin":
+
+    usuario5 = int(usuario5)
+    lista.append(usuario5)
+    
+    usuario5 = input("Ingrese un numero o ""fin"" si quiere terminar -> ")
+
+for m in lista:
+    suma = suma + m
+
+
+print("Ha terminado el programa")
+print(f"Este es el promedio de los numero ingresados {suma/m}")
+
+
+
+
+# Ejercicio 4
+
+
+def palabra()
+    
+
+
+# Ejercicio 5
+
+
+
+
+
+pepe = input('Escriba un parrafo -> ').split()
+
+diccionario20 = {}
+
+for g in pepe:
+
+    if g in diccionario20:
+        diccionario20[g] += 1
+    else:
+        diccionario20[g] = 1
+    
+print(diccionario20)
+
+
+# Ejercicio 6
+
+numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+suma = 0
+
+for n in numeros:
+
+    if n % 2 == 0:
+        suma += n
+
+print(f"La suma de los numeros pares es: {suma}")
+
+
+# Ejercicio 7
+
+
+print('Agenda de Contactos Simple 📔')
+
+opciones = (1, 2, 3, 4)
+
+contactos = {}
+
+kj = input('1. Añadir un contacto. \n2. Buscar un contacto. \n3. Listar todos los contactos. \n4. Salir. ')
+
+while kj != '4':
+
+    if kj not in opciones:
+        print('\nNinguna opcion es valida\n')
+
+    elif kj == '1':
+        ol = input('El nombre y el número de teléfono -> ')
+        contactos.update(ol)
+
+    elif kj == '2':
+
+
+
+    elif kj == '3':
+        if not contactos:
+            print('\nNo tienes nada, agrega algo\n')
+        else:
+            print('\nLista de contactos')
+            for i in range(len(contactos)):
+                print(f'{contactos}')
+            print()
+print('Haz salido correctamente')
+
+
+# Ejercicio 8
+lista1 = [1, 2, 5, 6, 'Hola', 'Mundo', 8, 'Futbol']
+lista2 = [6, 'Hola', 'Mundo', 8, 9, 0, 'Pepsi']
+
+set1 = set(lista1)
+set2 = set(lista2)
+
+tienen = set1.intersection(set2)
+
+print(f'{tienen}')
+
+
+# Ejercicio 9
+
+
+list_tuplas = [
+    ('nombre', 'Ana'), 
+    ('edad', 22), 
+    ('ciudad', 'Valencia')
+]
+
+diccionario = {}
+
+for clave, valor1 in list_tuplas:
+    diccionario[clave] = valor1
+    
+
+print(diccionario)
+
+
+
+
+# Ejercicio 10
+
+people = int(input('Ingrese un numero -> '))
+
+multi =  people * '*'
+
+for j in range(people):
+    print(f'{multi}')
+
