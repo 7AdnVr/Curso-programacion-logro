f05e8f12f2cf00911e5084edba4f3bd1deac7b73
7AdnVr <abokase4706@gmail.com>
2026-04-01 18:03:05 -0400

    v2

--- 0.py
+++ 0.py
@@ -12,25 +12,4 @@ longitud = 2 * math.pi * radio
 
 print(f"el area es {area:.2f} y su longittud es {longitud:.2f}")
 '''
-print(f'\nBienvenido al cajero automatico')
-saldo = 1000
-
-operacion = input('Que operacion desea realizar \n1.Ingresar dinero en la cuenta. \n2.Retirar dinero de la cuenta. \n3.Mostrar dinero disponible. \n4.Salir. \n-> ')
-
-if operacion == '1':
-    ingreso = int(input(f'Cuanto dinero desea ingresar -> '))
-    saldo += ingreso
-    print(f'Se ha depositado {ingreso}')
-elif operacion == '2':
-    retirar = int(input(f'Cuanto dinero desea retirar ->'))
-    if retirar <= saldo:
-        saldo -= retirar
-        print(f'Ha retirado {retirar}')
-    else:
-        print('No tienes money')
-elif operacion == '3':
-    print(f'Este su salgo -> {saldo}')
-elif operacion == '4':
-    print('Vuelva pronto')
-        
-    
+v2
