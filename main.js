ca45c4865ae5417cc37654b0fd1a69dae5e8fe98
7AdnVr <abokase4706@gmail.com>
2026-04-02 01:29:12 -0400

    v2

--- main.js
+++ main.js
@@ -35,10 +35,34 @@ const styleSwitcher = document.getElementById('style-switcher'),
 /* Switcher show */
 switcherToggle.addEventListener('click', () => {
     styleSwitcher.classList.add('show-switcher');
-})
+});
 
 /* Switcher hidden */
+switcherClose.addEventListener('click', () => {
+    styleSwitcher.classList.remove('show-switcher');
+});
 
 /*=============== THEME COLORS ===============*/
+const colors = document.querySelectorAll('.style-switcher-color');
+
+colors.forEach((color) => {
+    color.onclick = () => {
+        const activeColor = color.style.getPropertyValue('--hue')
+
+        colors.forEach((c) => c.classList.remove('active-color'));
+        color.classList.add('active-color');
+
+        document.documentElement.style.setProperty('--hue', activeColor);
+    };
+});
 
 /*=============== LIGHT/DARK MODE ===============*/
+let currentTheme = 'light';
+document.body.className = currentTheme;
+
+document.querySelectorAll('input[name="body=theme"]').forEach((input) => {
+    input.addEventListener('change', () => {
+        currentTheme = input.value;
+        document.body.className = currentTheme;
+    });
+});
