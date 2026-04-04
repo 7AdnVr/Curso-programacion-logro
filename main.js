e0cec6cd0fe1ea5577373b8c796511b2a24b1833
7AdnVr <abokase4706@gmail.com>
2026-04-04 03:20:07 -0400

    v2

--- main.js
+++ main.js
@@ -207,14 +207,14 @@ switcherClose.addEventListener('click', () => {
 const colors = document.querySelectorAll('.style-switcher-color');
 
 colors.forEach((color) => {
-    color.onclick = () => {
-        const activeColor = color.style.getPropertyValue('--hue')
+    color.addEventListener('click', () => {
+        const activeColor = getComputedStyle(color).getPropertyValue('--hue');
 
         colors.forEach((c) => c.classList.remove('active-color'));
         color.classList.add('active-color');
 
         document.documentElement.style.setProperty('--hue', activeColor);
-    };
+    });
 });
 
 /*=============== LIGHT/DARK MODE ===============*/
