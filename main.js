7fd288949d1682f80d20af621a00f5bca3ab3892
7AdnVr <abokase4706@gmail.com>
2026-04-04 00:21:41 -0400

    v2

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ navToggle.addEventListener('click', () => {
 /*=============== REMOVE MENU MOBILE ===============*/
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 20 
+    ? header.classList.add('bg-header') 
+    : header.classList.remove('bg-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
